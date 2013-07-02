f24d1a73de2db36af7415abadd4808f54279a559
Phil <github@exclsr.com>
2013-07-02 12:46:22 -0700

    Use Ace editor instead of textarea
    The Ace editor, http://ace.ajax.org, is a
    neat tool for editing code in a browser.
    
    It might be easier to use Ace as a base
    if we want to have basic styling in the
    notes. More importantly, I'm going to see
    if we can make hyperlinks as we type.
    
    After struggling with getting the ui-ace
    component to work, I gave up and used
    this plunker as a base:
    
    http://plnkr.co/edit/JJe0mf9RmDiSYkfahdnq
    
    As commitment-level is medium at this point,
    we're pointing to the ace.js hosted on GitHub
    for the time being.

--- public/js/directives.js
+++ public/js/directives.js
@@ -7,4 +7,46 @@ angular.module('notes.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  });
+  })
+  .directive("aceEditor", [
+  function() {
+    var Editor, Renderer;
+    Editor = ace.require("ace/editor").Editor;
+    Renderer = ace.require("ace/virtual_renderer").VirtualRenderer;
+    return {
+      restrict: "EA",
+      require: "ngModel",
+      replace: true,
+      template: "<div class=\"ace-container\"></div>",
+
+      link: function($scope, $el, attrs, model) {
+        var editor, session, updateViewValue;
+        editor = new Editor(new Renderer($el[0], "ace/theme/textmate"));
+
+        var renderer = editor.renderer;
+        renderer.setShowGutter(false);
+        renderer.setShowPrintMargin(false);
+
+        session = editor.getSession();
+
+        model.$render = function() {
+          return session.setValue(model.$modelValue);
+        };
+
+        var updateViewValue = function() {
+          if (!$scope.$$phase) {
+            return $scope.$apply(function() {
+              return model.$setViewValue(session.getValue());
+            });
+          };
+        };
+
+        session.on("change", updateViewValue);
+        return $scope.$on("$destroy", function() {
+          return editor.removeListener("change", updateViewValue);
+        });
+      }
+    };
+  }
+]);
+;
